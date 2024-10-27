ed8a9d9b216bf4beeac085c2ea7e0c0a9fa25159
boehm <boehm@quellwerke.de>
2024-10-27 19:56:10 +0100

    FIX: Installed new Frontend with Vue 3, Vuetify 3 and Testing

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,19 +1,28 @@
+import './assets/main.css'
+
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-// Components
 import App from './App.vue'
 import router from './router'
-import store from './store'
+
+// set up vuetify with theme and icons
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
 const vuetify = createVuetify({
   components,
-  directives,
-})
+  directives
+});
 
-createApp(App).use(vuetify).use(store).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(vuetify);
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
