2f68fc8cce5b43a184f59c959e6b4d8ee1330feb
boehm <boehm@quellwerke.de>
2024-10-28 12:16:11 +0100

    Added Laravel, moved Vue Frontend to Laravel Project, installed cucumber with cypress

---
+++ laravel/cypress.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'cypress'
+import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
+import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
+import { createEsbuildPlugin } from '@badeball/cypress-cucumber-preprocessor/esbuild';
+
+export default defineConfig({
+  e2e: {
+    async setupNodeEvents(on, config) {
+      const bundler = createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      });
+
+      on('file:preprocessor', bundler);
+      await addCucumberPreprocessorPlugin(on, config);
+
+      return config;
+    },
+    specPattern: 'cypress/e2e/cucumber/*.feature',
+    supportFile: 'cypress/support/index.js',
+    baseUrl: 'http://localhost:8000'
+  },
+});
---
+++ laravel/cypress/e2e/cucumber/example.feature
@@ -0,0 +1,5 @@
+Feature: Example feature
+
+Scenario: Visiting the home page
+  Given I visit the home page
+  Then I should see the welcome message
