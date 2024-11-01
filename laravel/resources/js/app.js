8853085319c23ee608e270f2b5a86f8a1e1af16e
boehm <boehm@quellwerke.de>
2024-11-01 18:18:22 +0100

    Added mdi fonts package

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -13,9 +13,18 @@ import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
+// Material Design Icons
+import '@mdi/font/css/materialdesignicons.css'
+
 const vuetify = createVuetify({
   components,
-  directives
+  directives,
+  theme: {
+    defaultTheme: 'dark',
+  },
+  icons: {
+    defaultSet: 'mdi',
+  },
 });
 
 const app = createApp(App)
