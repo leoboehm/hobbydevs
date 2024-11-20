66cc0360a69fe8db07caf9b1b4acc76c8b6984a4
leoboehm <boeleo425@gmail.com>
2024-11-20 10:02:13 +0100

    fixed logout function

--- laravel/resources/js/services/axios.js
+++ laravel/resources/js/services/axios.js
@@ -2,10 +2,8 @@ import axios from 'axios'
 
 // Fetch and set the CSRF token as a default header for future requests
 async function fetchCsrfToken() {
-    await axios.get('/sanctum/csrf-cookie')  // Ensure CSRF token is set in cookies
+    await axios.get('/sanctum/csrf-cookie')
 }
-
-await fetchCsrfToken()
 
 const apiClient = axios.create({
     baseURL: '/api',
@@ -17,6 +15,17 @@ const apiClient = axios.create({
     withCredentials: true,
 })
 
+apiClient.interceptors.response.use(
+    response => response.data, // Return response data as usual
+    error => {
+      if (error.response && error.response.status === 419) {
+        // If the CSRF token has expired (419 error), reload the page
+        window.location.reload();
+      }
+      return Promise.reject(error); // Reject the error as usual
+    }
+  );
+  
 // Fetch CSRF token before making requests
 fetchCsrfToken().then(() => {
     // Once the CSRF token is fetched, set the header
