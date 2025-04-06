66818d05b311a30d3aaebeb5ae8bcbdf7842ce5a
CueMaxX <ebhlert@gmail.com>
2025-04-06 11:50:49 +0000

    Create about page

--- laravel/resources/js/views/AboutView.vue
+++ laravel/resources/js/views/AboutView.vue
@@ -1,7 +1,16 @@
 <template>
     <div>
-        <h1>About Page</h1>
-        <p>This is the placeholder for the About page.</p>
+        <h1>HobbyDevs - About</h1>
+        <div style="width:100%; margin-left:0%;">
+            <h2>Vision</h2>
+            <p>Hobby Devs is about creating a collaborative space, where ideas meet development. We want to connect people with innovative ideas to hobby developers who can bring these visions to life. Users can post their project ideas and developers can apply, providing their budget and time frame. We want to create a space, where developers can ask each other for advice and help. Through ratings and reviews, the project idea can find the perfect developer, that will help you build your idea to reality.
+            <br>We want to create an easy application process, where project owners can submit forms detailing their needs and developers can apply to these projects. Developers will be able to create profiles, where they can state their experiences and skills, helping project owners understand what they offer. Through ratings and reviews, the process of choosing the perfect developer for projects is efficient and easy.
+            <br>Once a project begins, users can track progress through a visible time frame that outlines tasks and deadlines. In a chat function developers and project owners can talk about more details and be more comfortable</p>
+            <h2>Blog</h2>
+            <p>You can find our project blog here: <a href="https://hobbydevs.wordpress.com/">Blog</a></p>
+            <h2>Miro Board</h2>
+            <p>We plan and brainstorm everything on Miro. You can find our board here: <a href="https://miro.com/welcomeonboard/a3pPelhaaUs4RGRLRkN0MnRLZjIwVGx0UzFzM2xBWVFJVTNZMVduR0pNS0VTMzFIMHZCeVBvUWtLMlY3eTFUV3wzNDU4NzY0NTcyMTQ3ODA2MDc4fDI=?share_link_id=164227986547">Miro Board</a></p>
+        </div>
     </div>
 </template>
 
