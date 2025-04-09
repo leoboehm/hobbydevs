5de420da7e6643a12fa583392d918360bc8cbdd9
leoboehm <boeleo425@gmail.com>
2025-04-09 16:29:47 +0200

    Changed design of projects view

--- laravel/resources/js/views/ProjectsView.vue
+++ laravel/resources/js/views/ProjectsView.vue
@@ -1,71 +1,86 @@
 <template>
-    <div>
-        <h2 class="text-green my-2">
+    <v-container fluid class="py-12">
+        <h2 class="text-center text-h5 font-weight-bold mb-8">
             These projects are currently looking for Developers:
         </h2>
-        
 
-        <v-row dense v-for="project in projectList" :key="project.id">
+        <v-row
+            dense
+            v-for="project in projectList"
+            :key="project.id"
+            class="mb-6"
+        >
             <v-col cols="12">
-                <v-card>
-                    <v-card-title>{{ project.title }}</v-card-title>
+                <v-card class="elevation-2">
+                    <v-card-title class="text-h6 font-weight-bold">{{
+                        project.title
+                    }}</v-card-title>
+                    <v-card-subtitle class="text-subtitle-2"
+                        >Category: {{ project.category }}</v-card-subtitle
+                    >
                     <v-card-text>
-                        Category: {{ project.category }}<br />
-                        Required Skills: {{ project.skills }}<br />
+                        <p class="text-subtitle-2">
+                            Required Skills: {{ project.skills }}
+                        </p>
                     </v-card-text>
+
                     <v-card-actions>
-                        <v-btn 
-                            @click="viewProjectDetail(project.id)" 
-                            class="text-primary"
+                        <v-btn
+                            @click="apply(project.id)"
+                            class="text-secondary"
+                            outlined
                         >
-                            Project Detail
+                            <v-icon class="mr-2">mdi-application</v-icon> Apply
+                            to this project
                         </v-btn>
                         <v-spacer />
-                        <v-btn 
-                            @click="apply(project.id)" 
+                        <v-btn
+                            @click="viewProjectDetail(project.id)"
                             class="text-primary"
-                        >
-                            Apply to this project
+                            outlined
+                            >Read more
+                            <v-icon class="ml-2">mdi-arrow-right</v-icon>
                         </v-btn>
                     </v-card-actions>
                 </v-card>
             </v-col>
         </v-row>
-    </div>
+    </v-container>
 </template>
 
 <script>
 import { useProjectStore } from '../stores/project'
 
 export default {
-    name: 'HomeView',
+    name: 'ProjectsView',
 
-  data() {
+    data() {
         return {
             projectStore: useProjectStore(),
-            projectList: []
-        };
+            projectList: [],
+        }
     },
 
     async created() {
         try {
-            const fetchedProjects = await this.projectStore.actionGetAllProjects();
+            const fetchedProjects =
+                await this.projectStore.actionGetAllProjects()
             if (fetchedProjects.length > 0) {
-                this.projectList = fetchedProjects; 
+                this.projectList = fetchedProjects
             }
-            console.log("Projects Loaded:", this.projectList);
+            console.log('Projects Loaded:', this.projectList)
         } catch (error) {
-            console.error("Error loading projects:", error);
+            console.error('Error loading projects:', error)
         }
     },
 
     methods: {
         viewProjectDetail(id) {
-            this.$router.push({ name: 'ProjectDetail', params: { id } });
+            this.$router.push({ name: 'ProjectDetail', params: { id } })
         },
         apply(id) {
-            this.$router.push({ name: 'Apply', params: { projectId: id } });
+            this.$router.push({ name: 'Apply', params: { projectId: id } })
         },
     },
-};
+}
 </script>
