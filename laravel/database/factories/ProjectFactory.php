3f5ed85670c424ce39f1a47e22f66c1d73ef3d49
leoboehm <boeleo425@gmail.com>
2025-05-21 10:13:27 +0200

    Added correct skill and category arrays to project factory

--- laravel/database/factories/ProjectFactory.php
+++ laravel/database/factories/ProjectFactory.php
@@ -20,14 +20,16 @@ class ProjectFactory extends Factory
         $salaryRanges = ['10-20k', '20-30k', '30-50k', '50-70k'];
         
         // Define categories
-        $categories = ['Web Development', 'Graphic Design', 'Content Writing', 'Marketing'];
+        $categories = ['Web Development', 'Mobile Development', 'Desktop Development', 'Game Development', 'Data Science', 'Machine Learning', 'Artificial Intelligence', 'Cyber Security', 'Networking'];
+
+        $skills = ['HTML', 'CSS', 'JavaScript', 'PHP', 'Python', 'Java', 'C#', 'C++', 'Ruby', 'Swift', 'Kotlin', 'Dart', 'React', 'Vue'];
 
         return [
             'owner_id' => 1,
             'title' => $this->faker->sentence(6, true),
             'description' => $this->faker->paragraph(4, true),
             'category' => $this->faker->randomElement($categories),
-            'skills' => json_encode($this->faker->words(5)),
+            'skills' => json_encode($this->faker->randomElement($skills)),
             'salary_range' => $this->faker->randomElement($salaryRanges),
             'duration' => $this->faker->numberBetween(1, 12) . ' months',
             'start_date' => $this->faker->dateTimeBetween('+1 week', '+1 month'),
