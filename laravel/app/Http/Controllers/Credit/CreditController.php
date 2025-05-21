4b10d8bb8ae2871d610911f43a14e69fb6071ad2
CueMaxX <ebhlert@gmail.com>
2025-05-21 20:35:46 +0000

    setup creditController.php

---
+++ laravel/app/Http/Controllers/Credit/CreditController.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CreditController extends Controller
+{
+    public function buyCredits(Request $request)
+    {
+        $user = Auth::user();
+        $amount = $request->input('amount', 10); // default 10 credits
+        // Fake payment logic here
+        $user->credits += $amount;
+        $user->save();
+
+        return response()->json(['credits' => $user->credits]);
+    }
+
+    public function spendCredits(Request $request)
+    {
+        $user = Auth::user();
+        $amount = $request->input('amount', 1);
+        if ($user->credits < $amount) {
+            return response()->json(['error' => 'Not enough credits'], 400);
+        }
+        $user->credits -= $amount;
+        $user->save();
+
+        return response()->json(['credits' => $user->credits]);
+    }
+}
