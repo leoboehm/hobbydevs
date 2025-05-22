2a5465c8f68d4340d56a706d967d6c2417eaab7f
CueMaxX <ebhlert@gmail.com>
2025-05-22 06:10:18 +0000

    push changes

--- laravel/vite.config.js
+++ laravel/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     server: {
         host: '127.0.0.1',
         port: 5173,
+        cors: true,
         proxy: {
             '/api': 'http://127.0.0.1:8000',
         },
